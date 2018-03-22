b32f2bac5a9f9ee01aade9a072696cd8bde3d0b0
oaishllib <secret@billhsiao.com>
2018-03-22 00:13:28 -0700

    controller/router

--- controller/task.js
+++ controller/task.js
@@ -1,9 +1,70 @@
 const task = require('../model/task')
 
-const index = (req, res, next) => {
-  res.render('index', { title: 'Express' });
+const index = (req, res) => {
+  Task.find({}, (err, tasks) => {
+    res.render('tasks/index', {tasks: tasks})
+  })
 }
 
+const newTask = (req, res) => {
+  res.render('tasks/new', {task: {}});
+}
+
+const create = (req, res) => {
+  if (req.body.cast) req.body.cast = req.body.cast.split(',');
+  for (var key in req.body) {
+    if (req.body[key] === '') delete req.body[key];
+  }
+  var task = new Task(req.body);
+  task.save(function(err) {
+    if (err) return res.render('tasks/new');
+    console.log(task);
+    res.redirect('/tasks/new');
+  });
+}
+
+
+const edit = (req, res, next) => {
+  Task.findById(req.params.id, (err, task) => {
+    if (err) return next(err);
+    res.render('tasks/edit', {task});
+  });
+}
+
+const update = (req, res) => {
+  Task.findByIdAndUpdate(req.params.id, req.body, function(err, task) {
+    if (err) return res.render(`tasks/'${req.params.id}'/edit`);
+    res.redirect('/tasks');
+  });
+}
+
+const remove = (req, res) => {
+  Task.findByIdAndRemove(req.params.id, function(err) {
+    res.redirect('/tasks');
+  });
+}
+
+const expand = (req, res, next) => {
+  Task.findById(req.params.id, function(err, task) {
+    if (err) return next(err);
+    res.render('tasks/expand', {task});
+  });
+}
+const detail = (req, res) => {
+  Task.findById(req.params.id, req.body, function(err, task) {
+    if (err) return res.render(`tasks/'${req.params.id}'/expand`);
+    res.redirect('/tasks');
+  });
+}
+
+
 module.exports = {
-  index
+  index: index,
+  new: newTask,
+  create: create,
+  edit: edit,
+  update: update,
+  remove: remove,
+  expand: expand,
+  detail: detail
 }
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,12 @@ const taskController = require('../controller/task')
 
 /* GET home page. */
 router.get('/', taskController.index);
+router.get('/new', taskController.new);
+router.post('/', taskController.create);
+router.get('/:id/edit', taskController.edit);
+router.put('/:id', taskController.update);
+router.delete('/:id', taskController.remove);
+router.get('/:id/expand', taskController.expand);
+router.put('/:id', taskController.detail)
 
 module.exports = router;
